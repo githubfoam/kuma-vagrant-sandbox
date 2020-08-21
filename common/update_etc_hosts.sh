4603aef83188952e288a356383bf59c3220886a9
githubfoam <lequestionis@gmail.com>
2020-08-21 09:25:47 +0300

    reinit

--- common/update_etc_hosts.sh
+++ common/update_etc_hosts.sh
@@ -7,6 +7,15 @@ set -o xtrace
 
 echo "======================update hosts==================================================================="
 
+export KUMA_CONTROL_PLANE_IP      = "192.168.33.10"
+export FRONTEND_IP                = "192.168.33.20"
+export BACKEND_IP                 = "192.168.33.30"
+export BACKEND_V1_IP              = "192.168.33.40"
+export POSTGRESQL_IP              = "192.168.33.50"
+export REDIS_IP                   = "192.168.33.60"
+export KONG_IP                    = "192.168.33.70"
+export METRICS_IP                 = "192.168.33.80"
+
 if [ -z "${KUMA_CONTROL_PLANE_IP}" ]; then
   echo "Error: environment variable KUMA_CONTROL_PLANE_IP is not set"
   exit 1
