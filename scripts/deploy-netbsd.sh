38ecaa0004a59ed8512c195cb2812f4234b4abae
githubfoam <lequestionis@gmail.com>
2020-08-23 13:56:40 +0300

    vg-netbsd-02 removed

--- scripts/deploy-netbsd.sh
+++ scripts/deploy-netbsd.sh
@@ -101,12 +101,12 @@ rm Vagrantfile
 # vagrant up --provider=libvirt "vg-netbsd-01"
 
 # https://app.vagrantup.com/NetBSD/boxes/NetBSD-8.0
-vagrant box add "NetBSD/NetBSD-8.0" --provider=virtualbox
-vagrant mutate "NetBSD/NetBSD-8.0" libvirt
-vagrant init --template Vagrantfile.provision.bash.netbsd.erb
-vagrant up --provider=libvirt "vg-netbsd-02"
-vagrant destroy -f "vg-netbsd-02"
-rm Vagrantfile
+# vagrant box add "NetBSD/NetBSD-8.0" --provider=virtualbox
+# vagrant mutate "NetBSD/NetBSD-8.0" libvirt
+# vagrant init --template Vagrantfile.provision.bash.netbsd.erb
+# vagrant up --provider=libvirt "vg-netbsd-02"
+# vagrant destroy -f "vg-netbsd-02"
+# rm Vagrantfile
 # vagrant ssh vgnode04 -c "hostnamectl"
 
 vagrant box list #veridy installed boxes
